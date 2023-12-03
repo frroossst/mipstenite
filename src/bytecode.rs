963f053f6d23169071cd3ca3bf157369570c5500
Adhyan H <adhyanpatel@gmail.com>
2023-12-03 06:21:23 -0600

    basic instructions

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,58 +1,75 @@
 use crate::registers::{Register, register_to_addr};
 
 pub enum Bytecode {
-    // loads register values
-    LOAD(u32),
-    // stores register values
-    STORE(u32),
-    // sets value to memory
-    MEMSET(u32),
-    // gets value from memory
-    MEMGET(u32),
-
-    ADD,
-    SUB,
-
-    JUMP(u32),
-
-    PUSH(Value),
-    POP(Value),
-
-    HALT,
+    // VM Specific
+    // =======================
+    ALLOC,
     DEBUG,
+    EXIT,
+    HALT,
+    STDIN,
+    STDOUT,
+    STRACE,
+    PUSH(Value),
+    POP,
+    // gets and pushes value to stack
+    GETP(Value),
+    // sets and pops value to register
+    SETO(Value),
+
+    // Register Specific
+    // =======================
+    SET(Value),
+
+    // Arithmetic Specifc
+    // =======================
+    ADD,
+
+
 }
 
 pub enum Value {
-    Register(Register),
+    Register(u32),
     Immediate(u32),
 }
 
 pub enum AsmInstruction {
     LI(String, u32),
-    ADD(u32, Value, Value),
+    ADD(String, String, String),
 }
 
 impl AsmInstruction {
 
     pub fn to_bytecode(&self) -> Vec<Bytecode> {
-        match self {
-            AsmInstruction::LI(reg, val) => {
-                return Self::convert_li(*self);
-            }
-            AsmInstruction::ADD(rd, rs, rt) => {
-                vec![
-                    Bytecode::PUSH(*rt),
-                    Bytecode::PUSH(*rs),
-                    Bytecode::ADD,
-                    Bytecode::POP(*rd),
-                ]
-            }
+        match *self {
+            AsmInstruction::LI(reg, imm) => {
+                let reg_name = register_to_addr(reg).expect("invalid register name: {reg}");
+                Self::convert_li(reg_name, imm)
+            },
+            AsmInstruction::ADD(src, op1, op2) => {
+                let reg_name = register_to_addr(src).expect("invalid register name: {src}");
+                let op1_name = register_to_addr(op1).expect("invalid register name: {op1}");
+                let op2_name = register_to_addr(op2).expect("invalid register name: {op2}");
+                Self::convert_add(reg_name, op1_name, op2_name)
+            },
             _ => { unimplemented!() }
         }
     }
 
-    fn convert_li() -> Vec<Bytecode> {
+    fn convert_li(reg: u32, imm: u32) -> Vec<Bytecode> {
+        vec![
+            Bytecode::PUSH(Value::Immediate(imm)),
+            Bytecode::SET(Value::Register(reg)),
+        ]
+    }
 
+    fn convert_add(reg: u32, op1: u32, op2: u32) -> Vec<Bytecode> {
+        vec![
+            Bytecode::GETP(Value::Register(op1)),
+            Bytecode::GETP(Value::Register(op2)),
+            Bytecode::ADD,
+            Bytecode::SETO(Value::Register(reg)),
+        ]
     }
     
 }
